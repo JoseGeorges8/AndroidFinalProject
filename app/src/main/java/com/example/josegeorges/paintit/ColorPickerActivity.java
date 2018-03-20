81dd5f13e375d6e364e69f4509bd258e13413795
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2018-03-20 10:13:11 -0400

    #13 working on the ColorPickerActivity, I added the views needed to make the seekBars from the RGBFragment work. next step is the view pager to display them

--- app/src/main/java/com/example/josegeorges/paintit/ColorPickerActivity.java
+++ app/src/main/java/com/example/josegeorges/paintit/ColorPickerActivity.java
@@ -1,8 +1,13 @@
 package com.example.josegeorges.paintit;
 
 import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
 
 
 /**
@@ -10,14 +15,51 @@ import android.os.Bundle;
  */
 public class ColorPickerActivity extends AppCompatActivity implements RGBFragment.OnFragmentInteractionListener {
 
+    //properties needed
+
+    Toolbar toolbar; //custom Action Bar
+
+    int redValue; //red value of the RGB
+    int greenValue; //green value of the RGB
+    int blueValue; //blue value of the RGB
+
+    //TODO: camera activity
+    FloatingActionButton fab; //for launching the camera activity
+    FrameLayout layout; //holds the color value that the user wants
+    ViewPager viewPager; //viewpager to show the fragments
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
+
+
+        //set up the toolbar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //quit the activity when the user hits the X at the top left corner
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //set up the frame for the color value
+        layout = findViewById(R.id.frame_layout);
+
+        //default colors
+        redValue = 0;
+        greenValue = 0;
+        blueValue = 0;
+
+
     }
 
+
     @Override
-    public void onFragmentInteraction(Uri uri) {
+    public void onFragmentInteraction(int value, String key) {
 
     }
 }
