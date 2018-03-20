8aa66823dca54d8c487348795ace88e3b3953e66
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2018-03-20 10:02:55 -0400

    #13 linked the seekbars to the fragment

--- app/src/main/java/com/example/josegeorges/paintit/RGBFragment.java
+++ app/src/main/java/com/example/josegeorges/paintit/RGBFragment.java
@@ -7,64 +7,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RGBFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RGBFragment#newInstance} factory method to
- * create an instance of this fragment.
+
+    Fragment will serve to pass the values from the SeekBars onto the Activity
+
  */
 public class RGBFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    //keys to identify colors while passing them to the activity
+    public static final String RED = "red";
+    public static final String GREEN = "green";
+    public static final String BLUE = "blue";
 
+    //listener
     private OnFragmentInteractionListener mListener;
+
+    //RGB Bars
+    SeekBar redBar;
+    SeekBar greenBar;
+    SeekBar blueBar;
 
     public RGBFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RGBFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RGBFragment newInstance(String param1, String param2) {
-        RGBFragment fragment = new RGBFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rgb, container, false);
+        View view = inflater.inflate(R.layout.fragment_rgb, container, false);
+
+
+        //views
+        redBar = view.findViewById(R.id.red_value);
+        greenBar = view.findViewById(R.id.green_value);
+        blueBar = view.findViewById(R.id.blue_value);
+
+
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
