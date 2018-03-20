e1ff53df1eb6b9b75b6346e450c0712c4fc5a5da
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2018-03-20 09:55:09 -0400

    #12 added the first fragment for the view pager. It's an rgb fragment that will contain three seek bars

--- app/src/main/java/com/example/josegeorges/paintit/ColorPickerActivity.java
+++ app/src/main/java/com/example/josegeorges/paintit/ColorPickerActivity.java
@@ -1,5 +1,6 @@
 package com.example.josegeorges.paintit;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,11 +8,16 @@ import android.os.Bundle;
 /**
  * This activity is meant for the user to chose custom colors and add them to their favourites or order them
  */
-public class ColorPickerActivity extends AppCompatActivity {
+public class ColorPickerActivity extends AppCompatActivity implements RGBFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
