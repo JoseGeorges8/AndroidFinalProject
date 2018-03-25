b829af0887ca5d8c39c2623c5934e30ab6710c13
Keegan Oribine <keegan.oribine01@stclairconnect.ca>
2018-03-25 00:31:34 -0400

    Created column names for each of the tables: #24

--- app/src/main/java/com/example/josegeorges/paintit/DatabaseHandler.java
+++ app/src/main/java/com/example/josegeorges/paintit/DatabaseHandler.java
@@ -10,19 +10,16 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    /**
-     * Keep track of the database version
-     */
+    // Database version
     public static final int DATABASE_VERSION = 1;
 
-    /**
-     * Create the name of the database
-     */
-    public static final String DATABASE_NAME = "hikingclub";
+    // name the database
+    public static final String DATABASE_NAME = "paintit";
 
     /**
-     * Create the names of all the tables
+     * Create names for each of the tables
      */
+
     public static final String TABLE_USERS = "user";
     public static final String TABLE_ORDERS = "order";
     public static final String TABLE_ITEMS = "item";
@@ -32,6 +29,67 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public static final String TABLE_PALETTECOLORS = "palette_color";
     public static final String TABLE_FAVORITECOLORS = "favorite_color";
     public static final String TABLE_FAVORITEPALETTES = "favorite_palette";
+
+    /**
+     * Create column names for each of the tables
+     *
+     */
+
+    // Users Table
+    public static final String COLUMN_USERID = "user_id";
+    public static final String COLUMN_FIRSTNAME = "first_name";
+    public static final String COLUMN_LASTNAME = "last_name";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_PASSWORD = "password";
+    public static final String COLUMN_RECOVERYEMAIL = "recovery_email";
+    public static final String COLUMN_PHONENUMBER = "phone_number";
+
+    // Orders Table
+    public static final String COLUMN_ORDERID = "order_id";
+    public static final String COLUMN_ORDERNUMBER = "order_number";
+    public static final String COLUMN_DATEORDERED = "date_ordered";
+    // Uses USERID
+
+    // Items Table
+    public static final String COLUMN_ITEMID = "item_id";
+    public static final String COLUMN_UPC = "upc";
+    public static final String COLUMN_PRICE = "price";
+    public static final String COLUMN_DESCRIPTION = "description";
+
+    // OrderItems Table **** Linking Table ****
+    public static final String COLUMN_ORDERQUANTITY = "order_quantity";
+    // Uses ORDERID
+    // Uses ITEMID
+
+    // Palettes Table
+    public static final String COLUMN_PALETTEID = "palette_id";
+    public static final String COLUMN_PALETTENAME = "palette_name";
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+    // Uses USERID
+
+    // Colors Table
+    public static final String COLUMN_HEXVALUE = "hex_value";
+    public static final String COLUMN_COLORNAME = "color_name";
+    // Uses TIMESTAMP
+
+    /**
+     * PaletteColors Table **** Linking Table ****
+     * Uses PALETTEID and COLORID
+      */
+
+    /**
+     * FavoriteColors Table
+     * Uses USERID
+     * Uses COLORID
+     */
+
+    /**
+     * FavoritePalettes Table
+     * Uses USERID
+     * Uses PALETTEID
+     */
+    
+
 
     public DatabaseHandler(Context context){
         super(context, DATABASE_NAME, null , DATABASE_VERSION);
