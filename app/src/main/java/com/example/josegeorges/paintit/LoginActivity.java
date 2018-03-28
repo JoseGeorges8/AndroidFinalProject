1106dbc8d2510e670656672cc416c13b1d173fcc
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2018-03-27 22:55:01 -0400

    #29 because of some problems when displaying regusterFragment, I made a loginFragment to be hosted first.

--- app/src/main/java/com/example/josegeorges/paintit/LoginActivity.java
+++ app/src/main/java/com/example/josegeorges/paintit/LoginActivity.java
@@ -1,20 +1,32 @@
 package com.example.josegeorges.paintit;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements RegisterFragment.OnFragmentInteractionListener,
+            LoginFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.login_content, new LoginFragment())
+                .addToBackStack(null)
+                .commit();
+
+
     }
 
-    public void signAsGuest(View view){
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
