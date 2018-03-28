1c7eb6d65feef8470718a663d734a834998f9295
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2018-03-27 22:37:45 -0400

    #29 linked views with fragment

--- app/src/main/java/com/example/josegeorges/paintit/RegisterFragment.java
+++ app/src/main/java/com/example/josegeorges/paintit/RegisterFragment.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
 /**
@@ -18,35 +22,26 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class RegisterFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    //Views
+    private ImageView cancel;
+    private EditText fname;
+    private EditText lname;
+    private EditText email;
+    private EditText password;
+    private EditText recoveryEmail;
+    private EditText number;
 
     public RegisterFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static RegisterFragment newInstance(String param1, String param2) {
         RegisterFragment fragment = new RegisterFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,8 +50,7 @@ public class RegisterFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -64,7 +58,31 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        //to close the fragment
+        cancel = view.findViewById(R.id.register_cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().onBackPressed();
+            }
+        });
+
+
+        //edit text fields
+        fname = view.findViewById(R.id.register_fname);
+        lname = view.findViewById(R.id.register_lname);
+        email = view.findViewById(R.id.register_email);
+        password = view.findViewById(R.id.register_password);
+        recoveryEmail = view.findViewById(R.id.register_recovery_email);
+        number = view.findViewById(R.id.register_number);
+
+        return view;
+    }
+
+    public void registerUser(View view){
+        Log.d("REGISTER", "pressed");
     }
 
     // TODO: Rename method, update argument and hook method into UI event
