08f83325d296379ff3624802823bcc97798c5e45
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2018-03-30 13:36:43 -0400

    #38 created a settingsScreenFragment that adds the preferences xml

---
+++ app/src/main/java/com/example/josegeorges/paintit/SettingsScreenFragment.java
@@ -0,0 +1,22 @@
+package com.example.josegeorges.paintit;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by josegeorges on 2018-03-30.
+ */
+
+/**
+ * Displays tthe settings for the app
+ */
+public class SettingsScreenFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
