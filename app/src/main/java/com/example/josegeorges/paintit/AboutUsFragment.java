3f4d4a52a65651b96bd7c22e8ec1278490961284
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2018-04-19 12:59:59 -0400

    #62 added intents for calling and emailing

--- app/src/main/java/com/example/josegeorges/paintit/AboutUsFragment.java
+++ app/src/main/java/com/example/josegeorges/paintit/AboutUsFragment.java
@@ -1,12 +1,15 @@
 package com.example.josegeorges.paintit;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
 public class AboutUsFragment extends Fragment {
@@ -30,7 +33,31 @@ public class AboutUsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about_us, container, false);
 
+        LinearLayout callUs = view.findViewById(R.id.phone_container);
+        callUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + "5192222222"));
+                if(intent.resolveActivity(getActivity().getPackageManager()) != null){
+                    getActivity().startActivity(intent);
+                }
+            }
+        });
 
+        LinearLayout emailUs = view.findViewById(R.id.email_container);
+        emailUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] emailAddress = {"customerservice@paintit.ca"};
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL, emailAddress);
+                if(intent.resolveActivity(getActivity().getPackageManager()) != null){
+                    getActivity().startActivity(intent);
+                }
+            }
+        });
 
         return view;
     }
